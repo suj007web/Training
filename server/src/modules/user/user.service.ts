0a319eb1f6b9949a0bd2a3999d29b4ea96716c24
Sujal Chauhan <sujal.chauhan@myrealdata.in>
2025-08-04 18:21:53 +0530

    feat : add password service

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,23 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import type { UserRepository } from 'src/domains/user/user.repository';
 import { RegisterDTO } from './dto/register.dto';
-import { User } from 'src/domains/user/user.entity';
+import { User, UserRole } from 'src/domains/user/user.entity';
+import { LoginDTO } from './dto/login.dto';
+import type { PasswordService } from 'src/domains/common/password.service';
 
 @Injectable()
 export class UserService {
 
     constructor(
-        private readonly userRepository : UserRepository
+        private readonly userRepository : UserRepository,
+        private readonly passwordService: PasswordService
     ){}
 
 
     async createUser(registerDto : RegisterDTO) : Promise<Partial<User>>{
-        const user = new User(
-            registerDto.username,
-            registerDto.email,
-            registerDto.password,
-            registerDto.role = registerDto.role || UserRole.USER
-        );
+        const hashedPassword = await this.passwordService.hashPassword(registerDto.password);
+        const user : User = {
+            username: registerDto.username,
+            email: registerDto.email,
+            password: hashedPassword,
+            role: UserRole.USER
+        }
+
+        return this.userRepository.create(user);
     }
+
+    async login(loginDto : LoginDTO) : Promise<Partial<User> | null> {
+        const username = loginDto.username;
+        const user = await this.userRepository.findByData({
+            username : username
+        })
+
+        if(!user || !(await this.passwordService.verifyPassword(loginDto.password, user.password))) {
+            return null; 
+        }
+
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+
 
 }
